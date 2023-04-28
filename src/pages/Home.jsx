e85d4769bdfd7a2810ba7f4d9a109ab72d09188e
alparslan-2010 <alparslannihatt@gmail.com>
2023-04-28 22:34:13 +0200

    blog

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Header title="clean blog" description="lorem" img="home"/>
+    </div>
+  )
+}
